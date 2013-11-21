bb377bae19437f6ab374131a8b3e220e334722a9
Bill Schuller <bill.schuller@gmail.com>
2013-11-21 14:58:37 -0600

    Enhancements
    Only select the FIRST createDate. Turns out some exif list more than
    one.
    
    Rather than be specific about a person when deciding to annotate
    date/weight, just see if the Weight data exists first.
    
    Modified imagemagick commands to fill the entire area of
    finalResolutiion
    
    Fixed the situation when an exact match doesn't exist for a creation
    date in the weight CSV.

--- fatface.bash
+++ fatface.bash
@@ -7,15 +7,17 @@
 CF=.fatface.exifcache
 finalResolution=640x640
 fontSize=72
-
-#for P in IMG_2972.jpg IMG_2584.jpg IMG_2006.jpg; do
+units=standard
+#set -x
+#for P in IMG_2831.jpg; do
 for P in `ls *.jpg`; do
 	echo "Processing $P..."
 	unset names
 	exiftool $P > $CF
 	rDH=`grep 'Region Applied To Dimensions H' $CF | cut -d: -f2`
 	rDW=`grep 'Region Applied To Dimensions W' $CF | cut -d: -f2`
-	rawCreateDate=`grep 'Create Date' $CF | cut -d: -f2-4 | cut -d' ' -f2 | tr ':' ','`
+	rawCreateDate=`grep -m 1 'Create Date' $CF | cut -d: -f2-4 | cut -d' ' -f2 | tr ':' ','`
+	#echo "   rawCreateDate is $rawCreateDate"
 	rH=`grep 'Region Area H' $CF | cut -d: -f2 | tr -d ' '`
 	rW=`grep 'Region Area W' $CF | cut -d: -f2 | tr -d ' '`
 	rX=`grep 'Region Area X' $CF | cut -d: -f2 | tr -d ' '`
@@ -27,6 +29,7 @@ for P in `ls *.jpg`; do
 	IFS=,
 	idx=0
 	for dateComponent in $rawCreateDate; do
+		#echo "    dateComponent is $dateComponent"
 		createDate[idx]=$dateComponent
 		((idx++))
 	done
@@ -61,10 +64,13 @@ for P in `ls *.jpg`; do
 	unset IFS
 
 	idx=0
-	#echo "Create date is ${createDate[0]}-${createDate[1]}-${createDate[2]}"
+	#echo "   Create date is ${createDate[0]}-${createDate[1]}-${createDate[2]}"
 	createDate[1]=`echo ${createDate[1]} | sed 's/^0*//'`
 	createDate[2]=`echo ${createDate[2]} | sed 's/^0*//'`
+	# createDate 0=year, 1=month 2=day
 	grepDate=${createDate[1]}\/${createDate[2]}\/${createDate[0]}
+	egrepDate="$createDate[1]"
+	#echo "   grepDate is $grepDate"
 	
 	#echo "grepDate is $grepDate"
 	#echo Names Index looks like this \"${!names[@]}\"
@@ -81,20 +87,42 @@ for P in `ls *.jpg`; do
 		a=+
 		u=_
 		c=cropped
+		carrot='^'
 		#echo "      $fH$m$fW$a$fX$a$fY"
-		if [ ${names[idx]}=="BillSchuller" ];then
-			echo `grep $grepDate ${names[idx]}.csv`
-			currentWeightKG=`grep $grepDate ${names[idx]}.csv | cut -d, -f3 | tr -d '"'`
+		if [ -f ${names[idx]}.csv ];then
+			#echo `grep -m 1 $grepDate ${names[idx]}.csv`
+			currentWeightKG=`grep -m 1 $grepDate ${names[idx]}.csv | cut -d, -f3 | tr -d '"'`
+			if [ -z "$currentWeightKG" ]
+			then
+			  currentWeightKG=0
+			fi
 			### Need to add a conditional here in case there is no weight for that date.
-			curretnWeightLBS=$(echo "$currentWeightKG*2.20462" | bc | cut -d. -f1)
-			convert $P -crop $fH$m$fW$a$fX$a$fY -resize $finalResolution $P$u${names[idx]}$u$c.jpg
-			convert $P$u${names[idx]}$u$c.jpg -pointsize $fontSize -fill white  -undercolor '#00000080'  -gravity South -annotate +0+5 "$curretnWeightLBS" $curretnWeightLBS$P$u${names[idx]}$u$c.jpg
+			if [ "$units" == "standard" ]; then
+				currentWeight=$(echo "$currentWeightKG*2.20462" | bc | cut -d. -f1)
+			fi
+			if [ "$currentWeight" == "0" ]; then
+				currentWeight="NULL"
+			fi
+
+			# round them corners!
+			
+			#convert -size 213x160 -stroke none -fill white -draw \"roundRectangle 50,50 167,110 10,10 \" label.png
+			convert $P -crop $fH$m$fW$a$fX$a$fY -resize $finalResolution$carrot -extent $finalResolution $P$u${names[idx]}$u$c.jpg
+			convert $P$u${names[idx]}$u$c.jpg \
+				-pointsize $fontSize \
+					-fill white \
+					-undercolor '#00000080'  \
+				-gravity SouthWest -annotate +0+5 "$currentWeight" \
+				-gravity SouthEast -annotate +0+5 "$grepDate"\
+				$currentWeight$P$u${names[idx]}$u$c.jpg
 			rm $P$u${names[idx]}$u$c.jpg
 		else
-			convert $P -crop $fH$m$fW$a$fX$a$fY -resize $finalResolution $P$u${names[idx]}$u$c.jpg
+			convert $P -crop $fH$m$fW$a$fX$a$fY -resize $finalResolution$carrot -extent $finalResolution $P$u${names[idx]}$u$c.jpg
 		fi
+		unset currentWeight 
 		((idx++))
 	done
+	unset createDate grepDate
 done
 
 
